09fcdf3163fa6cd4e102e6596507c14e449223ac
ledan404 <alfa7.plast@gmail.com>
2023-12-28 15:46:36 +0200

    6lab

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,21 @@ import MoreButtom from "./components/MoreButton";
 const App = () => {
   return (
     <main className="relative max-container">
-    <Header />
-    <section className="pt-40">
-    <Hero />
-    </section>
-    <section className="flex justify-center gap-16 pt-40">
-        < Item />
-        <Item / >
-        <Item / >
-    </section>
-    <section className="justify-center py-10 flex">
-      < MoreButtom />
-    </section>
-    <section>
-    <Fotter /> 
-    </section>
+      <Header />
+      <section className="pt-40">
+        <Hero />
+      </section>
+      <section className="flex justify-center gap-16 pt-40">
+        <Item />
+        <Item />
+        <Item />
+      </section>
+      <section className="justify-center py-10 flex">
+        <MoreButtom />
+      </section>
+      <section>
+        <Fotter />
+      </section>
     </main>
   );
 };
